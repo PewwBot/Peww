1495c172d34a4c4d598a281c858bbbbd320c57a0
Draww <drawwbeta@gmail.com>
2020-12-15 19:02:39 +0300

    added channel and member argument

---
+++ src/arguments/MemberArgument.ts
@@ -0,0 +1,23 @@
+import { AbstractArgument } from '../api/command/argument/AbstractArgument';
+
+import * as Discord from 'discord.js';
+import { CommandContext } from '../api/command/context/CommandContext';
+
+export class MemberArgument extends AbstractArgument<Discord.GuildMember> {
+  constructor() {
+    super({
+      name: 'member',
+    });
+  }
+
+  init(): void {
+    this.setupOptions({
+      regex: undefined,
+      shift: false,
+    });
+  }
+
+  async to(context: CommandContext, _controlledArg: string | undefined): Promise<Discord.GuildMember> {
+    return context.getMessage().member;
+  }
+}
